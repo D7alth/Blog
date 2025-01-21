7179ef9f162a6427ddd951aa23d3f13fdb2da620
Alberth Henrique <alberthhls@protonmail.com>
2025-01-21 13:16:58 -0300

    ✨ Added entity type configuration and added new changes on database

--- src/Infrastructure/Domain/Entities/Posts/PostEntityTypeConfiguration.cs
+++ src/Infrastructure/Domain/Entities/Posts/PostEntityTypeConfiguration.cs
@@ -1,6 +1,9 @@
 using Blog.Domain.Entities.Posts;
+using Blog.Domain.Entities.Posts.ValueObjects;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Newtonsoft.Json;
 
 namespace Blog.Infrastructure.Domain.Entities.Posts;
 
@@ -10,5 +13,31 @@ internal sealed class PostEntityTypeConfiguration : IEntityTypeConfiguration<Pos
     {
         builder.ToTable("posts");
         builder.HasKey(e => e.Id);
+        builder.Property(e => e.Title).HasColumnName("title").HasMaxLength(60);
+        builder.Property(e => e.CreatedAt).HasColumnName("created_at");
+        builder.Property(e => e.UpdatedAt).HasColumnName("updated_at");
+        builder
+            .Property(e => e.Tags)
+            .HasColumnType("text")
+            .HasColumnName("tags")
+            .HasConversion(
+                t => JsonConvert.SerializeObject(t.Select(tag => tag.Name).ToList()),
+                t =>
+                    JsonConvert
+                        .DeserializeObject<List<Tag>>(t)!
+                        .Select(tag => Tag.Create(tag.Name))
+                        .ToList()
+            )
+            .Metadata.SetValueComparer(
+                new ValueComparer<List<Tag>>(
+                    (a1, a2) => a1!.SequenceEqual(a2!),
+                    a =>
+                        a.Aggregate(
+                            0,
+                            (hash, item) => HashCode.Combine(hash, item.Name.GetHashCode())
+                        ),
+                    a => a.ToList()
+                )
+            );
     }
 }
--- src/Infrastructure/Persistence/Migrations/20250121161613_AddedTagsToPosts.Designer.cs
+++ src/Infrastructure/Persistence/Migrations/20250121161613_AddedTagsToPosts.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Blog.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Blog.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20250121161613_AddedTagsToPosts")]
+    partial class AddedTagsToPosts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
