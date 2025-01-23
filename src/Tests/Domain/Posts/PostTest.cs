e32836c38ded5fbf29a801be3104492b3418b878
Alberth Henrique <alberthhls@protonmail.com>
2025-01-22 22:04:07 -0300

    ✅ Added tests to Tag VO; Added new tests to Post

---
+++ src/Tests/Domain/Posts/PostTest.cs
@@ -0,0 +1,67 @@
+using Blog.Domain.Posts;
+using Bogus;
+
+namespace Blog.Tests.Domain.Posts;
+
+[Category("Post.Domain")]
+[TestFixture]
+public class PostTest
+{
+    private const string Title = "Any Title";
+    private const string Content = "Any Content";
+    private readonly List<string> _tags = ["books", "API", "Clean Code"];
+    private readonly Faker _faker = new();
+
+    [SetUp]
+    public void Setup() { }
+
+    [Test]
+    public void ShouldCreatePost()
+    {
+        var post = Post.Create(Title, Content);
+        Assert.Multiple(() =>
+        {
+            Assert.That(post.Title, Is.EqualTo(Title));
+            Assert.That(post.Content, Is.EqualTo(Content));
+        });
+    }
+
+    [Test]
+    public void ShouldAddTagToPost()
+    {
+        var post = Post.Create(Title, Content);
+        _tags.ForEach(post.AddTag);
+        Assert.Multiple(() =>
+        {
+            Assert.That(post.Tags.First().Name, Is.EqualTo(_tags.First()));
+            Assert.That(post.Tags.Last().Name, Is.EqualTo(_tags.Last()));
+        });
+    }
+
+    [Test]
+    public void ShouldNotAddDuplicateTagsToSamePost()
+    {
+        var post = Post.Create(Title, Content);
+        post.AddTag(_tags[0]);
+        post.AddTag(_tags[0]);
+        post.AddTag(_tags[0]);
+        Assert.Multiple(() =>
+        {
+            Assert.That(post.Tags, Has.Count.EqualTo(1));
+        });
+    }
+
+    [Test]
+    public void ShouldNotCreatePostWithEmptyTitle() =>
+        Assert.Throws<ArgumentException>(() => Post.Create("", Content));
+
+    [Test]
+    public void ShouldNotCreatePostWithTitleTooBig() =>
+        Assert.Throws<ArgumentException>(
+            () => Post.Create(_faker.Rant.Random.AlphaNumeric(90), Content)
+        );
+
+    [Test]
+    public void ShouldNotCreatePostWithEmptyContent() =>
+        Assert.Throws<ArgumentException>(() => Post.Create(Title, ""));
+}
---
+++ src/Tests/Domain/ValueObjects/TagTest.cs
@@ -0,0 +1,23 @@
+using Blog.Domain.Posts.ValueObjects;
+using Blog.Domain.Shared.Exceptions;
+
+namespace Blog.Tests.Domain.ValueObjects;
+
+[TestFixture]
+public class TagTest
+{
+    private const string TagName = "Books";
+    
+    [Test]
+    public void ShouldCreateTag()
+    {
+        var tag = Tag.Create(TagName);
+        Assert.That(tag.Name, Is.EqualTo(TagName));
+    }
+    
+    [Test]
+    public void ShouldThrowErrorWhenTagNameIsEmpty()
+    {
+        Assert.Throws<NullOrEmptyException>(() => Tag.Create(""));
+    }
+}
