e78f9406b4af9d96b1fa392925c29b9b2b9d0322
Alberth Henrique <alberthhls@protonmail.com>
2025-02-06 14:02:10 -0300

    ✨ Added new handlers to controller

--- src/API/Endpoints/Posts/PostModule.cs
+++ src/API/Endpoints/Posts/PostModule.cs
@@ -1,14 +1,34 @@
 using Blog.Application.Posts.Commands.Create;
+using Blog.Application.Posts.Commands.Delete;
 using Blog.Application.Posts.Queries.GetAll;
+using Blog.Application.Posts.Queries.GetById;
 using Carter;
 using MediatR;
 
 namespace Blog.API.Endpoints.Posts;
 
-public class PostModule() : CarterModule("/api/post")
+public class PostModule() : CarterModule("/api/posts")
 {
     public override void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapGet(
+            "{id:int}",
+            async (int id, IMediator mediator) =>
+            {
+                var post = await mediator.Send(new GetByIdQuery(id));
+                return Results.Ok(post);
+            }
+        );
+
+        app.MapGet(
+            "posts",
+            async (IMediator mediator) =>
+            {
+                var posts = await mediator.Send(new GetAllQuery());
+                return Results.Ok(posts);
+            }
+        );
+
         app.MapPost(
             "",
             async (CreateRequest request, IMediator mediator) =>
@@ -19,12 +39,13 @@ public class PostModule() : CarterModule("/api/post")
             }
         );
 
-        app.MapGet(
-            "",
-            async (IMediator mediator) =>
+        app.MapDelete(
+            "{id:int}",
+            async (int id, IMediator mediator) =>
             {
-                var posts = await mediator.Send(new GetAllQuery());
-                return Results.Ok(posts);
+                var command = new DeleteCommand(id);
+                await mediator.Send(command);
+                return Results.NoContent();
             }
         );
     }
