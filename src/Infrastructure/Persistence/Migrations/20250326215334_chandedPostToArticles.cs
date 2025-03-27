5900e859d41c9264beec140e10c172fb45469d88
Alberth Henrique <alberthhls@protonmail.com>
2025-03-26 22:47:59 -0300

    🔥 🚚 Move migrations to persistence directory and remove old GetAllArticlesTestQueryHandler

---
+++ src/Infrastructure/Persistence/Migrations/20250326215334_chandedPostToArticles.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class chandedPostToArticles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "posts");
+
+            migrationBuilder.RenameColumn(
+                name: "post_id",
+                table: "comments",
+                newName: "article_id"
+            );
+
+            migrationBuilder.CreateTable(
+                name: "articles",
+                columns: table => new
+                {
+                    Id = table
+                        .Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    title = table.Column<string>(
+                        type: "nvarchar(60)",
+                        maxLength: 60,
+                        nullable: true
+                    ),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    created_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    tags = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_articles", x => x.Id);
+                }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "articles");
+
+            migrationBuilder.RenameColumn(
+                name: "article_id",
+                table: "comments",
+                newName: "post_id"
+            );
+
+            migrationBuilder.CreateTable(
+                name: "posts",
+                columns: table => new
+                {
+                    Id = table
+                        .Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    created_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    tags = table.Column<string>(type: "text", nullable: false),
+                    title = table.Column<string>(
+                        type: "nvarchar(60)",
+                        maxLength: 60,
+                        nullable: true
+                    ),
+                    updated_at = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_posts", x => x.Id);
+                }
+            );
+        }
+    }
+}
